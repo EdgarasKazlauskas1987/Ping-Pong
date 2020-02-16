93aff5764aba16be2b1ee2f190f981bac07171c7
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2020-02-16 21:29:18 +0100

    Added moving for both players

--- src/virtual_pong/core.clj
+++ src/virtual_pong/core.clj
@@ -3,39 +3,54 @@
   (:require [quil.core :as quil]))
 
 
-(defn draw-rect-left [x y]
-  (quil/rect x y 30 120))
+(defn draw-rect-left [y]
+  (quil/rect 0 y 30 120))
 
-(defn draw-rect-right [x y]
-  (quil/rect x y 30 120))
+(defn draw-rect-right [y]
+  (quil/rect 770 y 30 120))
 
 (defn draw-rect-ball [x y]
   (quil/rect x y 35 35 100))
+
+(def player1-coordinates (atom {:y 0}))
+(def player2-coordinates (atom {:y 0}))
+
+(defn move-player1-up []
+  (swap! player1-coordinates update-in [:y] - 7))
+
+(defn move-player1-down []
+  (swap! player1-coordinates update-in [:y] + 7))
+
+(defn move-player2-up []
+  (swap! player2-coordinates update-in [:y] - 7))
+
+(defn move-player2-down []
+  (swap! player2-coordinates update-in [:y] + 7))
 
 (defn players-movement
   "Checking which key is pressed and calling corresponding function"
   [key]
   (cond
-    (= key :right) (println "Player A pressed :right key")
-    (= key :left) (println "Player A pressed :left key")
-    (= key :d) (println "Player B pressed :d key")
-    (= key :a) (println "Player B pressed :a key")))
+    (= key :right) (move-player2-up)
+    (= key :left) (move-player2-down)
+    (= key :d) (move-player1-down)
+    (= key :a) (move-player1-up)))
 
 (defn key-pressed
   "Function is activated when a key is pressed"
   []
-(when(quil/key-pressed?)
-  (players-movement(quil/key-as-keyword))))
+  (when (quil/key-pressed?)
+    (players-movement (quil/key-as-keyword))))
 
 (defn draw []
   (quil/background 11)
-  (draw-rect-left 0 0)
-  (draw-rect-right 770 0)
+  (draw-rect-left (get @player1-coordinates :y))
+  (draw-rect-right (get @player2-coordinates :y))
   (draw-rect-ball 100 100))
 
 (quil/defsketch pong
-  :title "Virtual Pong"
-  :size [800 450]
-  :draw (fn [] (draw))
-  :key-pressed key-pressed)
+                :title "Virtual Pong"
+                :size [800 450]
+                :draw (fn [] (draw))
+                :key-pressed key-pressed)
   
