e96fb79d45b1e24110a84a11b3d7b8a69ae1b8e7
Edgaras Kazlauskas <edgarasvilija@gmail.com>
2020-02-19 18:00:32 +0100

    Added button release detection

--- src/virtual_pong/core.clj
+++ src/virtual_pong/core.clj
@@ -1,6 +1,5 @@
 (ns virtual-pong.core
   (:gen-class)
-  (:import [java.awt.event KeyEvent])
   (:require [quil.core :as quil]))
 
 
@@ -15,6 +14,14 @@
 
 (def player1-coordinates (atom {:y 0}))
 (def player2-coordinates (atom {:y 0}))
+
+;; Add keys when they are pressed and remove when they are released
+(def player1-keys-collection (atom []))
+(def player2-keys-collection (atom []))
+
+;; When keys is pressed add it to this list. While the key is in the list
+;; move the player accordingly. When key is released remove it from the list.
+;; When the key it is no longer in the list the player will not be moved
 
 (defn move-player1-up []
   (swap! player1-coordinates update-in [:y] - 7))
@@ -43,6 +50,11 @@
   (when (quil/key-pressed?)
     (players-movement (quil/key-as-keyword))))
 
+(defn key-released
+  "Function is activated when a key is released"
+  []
+  (println (str "Released: " (quil/raw-key))))
+
 (defn draw []
   (quil/background 11)
   (draw-rect-left (get @player1-coordinates :y))
@@ -54,5 +66,6 @@
                 :size [800 450]
                 :setup (fn [] (quil/smooth) (quil/no-stroke) (quil/frame-rate 60))
                 :draw (fn [] (draw))
-                :key-pressed key-pressed)
+                :key-pressed key-pressed
+                :key-released key-released)
   
